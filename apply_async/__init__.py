1be60fdb0efff610d1c3da0f12d728e02a145ea8
Jeff Shen <shenjeff@princeton.edu>
2023-03-29 18:46:35 -0400

    bump version to 0.1.1

--- apply_async/__init__.py
+++ apply_async/__init__.py
@@ -2,7 +2,7 @@ __uri__ = "http://github.com/al-jshen/gaul"
 __author__ = "Jeff Shen"
 __email__ = "shenjeff@princeton.edu"
 __license__ = "MIT"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["apply_async"]
 
